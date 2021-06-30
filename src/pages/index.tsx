7d21d5fe4285e9298671587a88abde6a3ce9ff95
gabriel6181997 <gabriel6181997@gmail.com>
2021-06-30 22:08:21 +0800

    ✨ Add basic function for choosing album

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,23 @@
+const OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 const Home = () => {
   return (
-     <div className="max-w-screen-md mt-8 mx-auto text-center">
-       <h1 className="">Choose an Album</h1>
-     </div>
-  )
-}
+    <div className="max-w-screen-md mt-20 mx-auto text-center">
+      <h1 className="text-2xl font-bold">Choose an Album</h1>
+      <div className="mt-10">
+        <select>
+          {OPTIONS.map((option) => {
+            return (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            );
+          })}
+        </select>
+        <button className="ml-6">Enter</button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
