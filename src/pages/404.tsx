819aa3d92d304e7556dae017e459e381cc51b9d2
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-02 20:58:18 +0800

    ✨ Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+const Custom404 = () => {
+  return (
+    <div className="max-w-screen-md mt-20 mx-auto text-center">
+      <h1 className="text-2xl font-bold">このページは見つかりませんでした！</h1>
+      <p className="text-lg mt-5"> ページが削除されているか、URLが誤っている可能性があります。</p>
+    </div>
+  );
+};
+
+export default Custom404;
